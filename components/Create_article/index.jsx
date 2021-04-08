ef6d7ba9ad3fd8624684ea8779a1cf100e33d3a6
Fucao77 <axel.zidane@edu.devinci.fr>
2021-04-08 10:40:54 +0200

    first commit

---
+++ components/Create_article/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Button() {
+  <div>
+    <button>Crée un article</button>
+  </div>;
+}
---
+++ components/blog_admin/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from '../Create_article';
+
+export default function Blog() {
+  return (
+    <div>
+      <h1>Gérer le blog</h1>
+      <Button></Button>
+    </div>
+  );
+}
