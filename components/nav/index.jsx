27fdda4fe63552b11f94589595de544e4647d908
LucasO <=>
2021-04-08 10:46:05 +0200

    :construction: branch + connexion

--- components/nav/index.jsx
+++ components/nav/index.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
+import Link from 'next/link';
 import { nav } from './nav.module.scss';
 
 export default function Nav() {
-  return <nav className={nav}>Ceci est la nav</nav>;
+  return (
+    <nav className={nav}>
+      <Link href="/connexion/connexion.js">
+        <a>connexion</a>
+      </Link>
+      <Link href="/">
+        <a>Accueil</a>
+      </Link>
+      <Link href="/destination/destination.js">
+        <a>Destination</a>
+      </Link>
+      <Link href="/QSN/index.js">
+        <a>Qui sommes-nous?</a>
+      </Link>
+      <Link href="/faq/index.js">
+        <a>FAQ</a>
+      </Link>
+    </nav>
+  );
 }
