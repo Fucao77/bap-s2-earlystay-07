9133b76e21650cc347a9fad8b2d177a213d14e65
LucasO <=>
2021-04-27 18:57:49 +0200

    animation FAQ

--- components/FAQ/FAQ-button/index.jsx
+++ components/FAQ/FAQ-button/index.jsx
@@ -3,9 +3,19 @@ import { buttonfaq, text, container, svg } from './faq.module.scss';
 
 export default function FAQ({ title, children }) {
   const [isToggle, setToggle] = useState(false);
+  const [rotation, setRotation] = useState({ transform: 'rotate(0deg)' });
+
+  function changeRotation() {
+    if (!isToggle) {
+      setRotation({ transform: 'rotate(90deg)' });
+    } else {
+      setRotation({ transform: 'rotate(0deg)' });
+    }
+  }
 
   const toggle = () => {
     setToggle((prevState) => !prevState);
+    changeRotation();
   };
 
   return (
@@ -16,14 +26,15 @@ export default function FAQ({ title, children }) {
             {title}
             <svg
               className={svg}
-              width="16"
-              height="14"
-              viewBox="0 0 16 14"
+              style={rotation}
+              width="14"
+              height="16"
+              viewBox="0 0 14 16"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                d="M8 14L0.20577 0.5L15.7942 0.500002L8 14Z"
+                d="M14 8L0.500002 15.7942L0.500002 0.205772L14 8Z"
                 fill="#3668A4"
               />
             </svg>
