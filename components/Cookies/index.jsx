d25b8e9403d6af185dfb63f6d5b93880aba60f3c
Fucao77 <axel.zidane@edu.devinci.fr>
2021-04-29 14:12:28 +0200

    add_cookie

---
+++ components/Cookies/index.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+export default function Cookies() {
+  return (
+    <section>
+      {/*  <a href="#" id="changePreferences">Change your cookie preferences</a>
+  
+<script type="text/javascript" src="//www.cookieconsent.com/releases/3.1.0/cookie-consent.js"></script>
+<script type="text/javascript">
+    document.addEventListener('DOMContentLoaded', function () {
+        cookieconsent.run({
+            "notice_banner_type": "standalone",
+            "consent_type": "implied",
+            "palette": "light",
+            "language": "fr",
+            "website_name": "Earlystay",
+            "change_preferences_selector": "#changePreferences"
+        })
+    });
+</script>
+
+<script type="text/plain" cookie-consent="functionality" src="//www.cookieconsent.com/releases/3.1.0/cookie-consent.js">
+</script>
+<script type="text/plain" cookie-consent="functionality">
+    document.addEventListener('DOMContentLoaded', function () {
+cookieconsent.run({"notice_banner_type":"standalone","consent_type":"express","palette":"light","language":"fr","website_name":"Earlystay","change_preferences_selector":"#changePreferences"})
+});
+</script>
+
+<noscript>ePrivacy and GPDR Cookie Consent by <a href="https://www.CookieConsent.com/" rel="nofollow noopener">Cookie
+        Consent</a></noscript>
+
+
+
+<script type="text/plain" cookie-consent="targeting" src="//www.cookieconsent.com/releases/3.1.0/cookie-consent.js">
+</script>
+<script type="text/plain" cookie-consent="targeting">
+    document.addEventListener('DOMContentLoaded', function () {
+cookieconsent.run({"notice_banner_type":"standalone","consent_type":"express","palette":"light","language":"fr","website_name":"Earlystay","change_preferences_selector":"#changePreferences"})
+});
+</script>
+
+
+<script type="text/plain" cookie-consent="targeting" src="//www.cookieconsent.com/releases/3.1.0/cookie-consent.js">
+</script>
+<script type="text/plain" cookie-consent="targeting">
+    document.addEventListener('DOMContentLoaded', function () {
+cookieconsent.run({"notice_banner_type":"standalone","consent_type":"express","palette":"light","language":"fr","website_name":"Earlystay","change_preferences_selector":"#changePreferences"})
+});
+</script>
+
+<noscript>ePrivacy and GPDR Cookie Consent by <a href="https://www.CookieConsent.com/" rel="nofollow noopener">Cookie
+        Consent</a></noscript>
+
+
+<noscript>ePrivacy and GPDR Cookie Consent by <a href="https://www.CookieConsent.com/" rel="nofollow noopener">Cookie
+        Consent</a></noscript>
+
+
+
+
+
+<noscript>ePrivacy and GPDR Cookie Consent by <a href="https://www.CookieConsent.com/" rel="nofollow noopener">Cookie
+        Consent</a></noscript>
+*/}
+    </section>
+  );
+}
