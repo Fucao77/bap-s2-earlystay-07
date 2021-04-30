f0551489f42473deb2230487056e832bd0ae3dbd
Canberra Menthonnex <Canberra.menthonnex@edu.devinci.fr>
2021-04-30 11:24:53 +0200

    :art: fix card more article & responsive for article page

--- components/card-more-article/index.jsx
+++ components/card-more-article/index.jsx
@@ -12,13 +12,21 @@ import {
 export default function CardArticle({ id, img, description, date }) {
   const imgUrl = '/upload-image/article-image/' + img;
 
-  const descripTrunc = description.split(' ').splice(0, 70).join(' ') + '...';
+  const descriptionTrunc = description;
+
+  console.log(typeof descriptionTrunc);
+
+  console.log(id);
+
+  // if(descriptionTrunc.split(' ').length > 70){
+  //     descriptionTrunc = descriptionTrunc.split(' ').splice(0, 70).join(' ') + '...';
+  // }
 
   return (
     <>
       <article className={articleCard}>
         <img src={imgUrl} alt="image-more" className={imageCard} />
-        <p className={articleDescription}>{descripTrunc}</p>
+        <p className={articleDescription}>{descriptionTrunc}</p>
 
         <div className={block}>
           <div className={articleIcon}>
--- pages/articles/[id].js
+++ pages/articles/[id].js
@@ -18,12 +18,11 @@ import {
   h2Article,
 } from '../../styles/pages/article.module.scss';
 
-export default function article({ article, article1, article2 }) {
-  const srcMiniature = '/upload-image/article-image/' + article.miniature;
-
-  const dateParse = dateToString(new Date(Date.parse(article.created_at)));
-  const dateParse2 = dateToString(new Date(Date.parse(article1.created_at)));
-  const dateParse3 = dateToString(new Date(Date.parse(article2.created_at)));
+export default function article({
+  article,
+  firstMoreArticle,
+  secondMoreArticle,
+}) {
   function titleCase(str) {
     var splitStr = str.toLowerCase().split(' ');
     for (var i = 0; i < splitStr.length; i++) {
@@ -34,9 +33,22 @@ export default function article({ article, article1, article2 }) {
     return splitStr.join(' ');
   }
 
+  const srcMiniature = '/upload-image/article-image/' + article.miniature;
+
+  const dateParse = dateToString(new Date(Date.parse(article.created_at)));
+  const dateParse2 = dateToString(
+    new Date(Date.parse(firstMoreArticle.created_at))
+  );
+  const dateParse3 = dateToString(
+    new Date(Date.parse(secondMoreArticle.created_at))
+  );
+
   let date = titleCase(dateParse);
   let date2 = titleCase(dateParse2);
   let date3 = titleCase(dateParse3);
+
+  console.log(typeof firstMoreArticle.id);
+
   return (
     <>
       <Nav />
@@ -58,19 +70,23 @@ export default function article({ article, article1, article2 }) {
       <h2 className={h2Article}>Articles Similaires :</h2>
 
       <section className={sectionMoreArticle}>
-        <CardArticle
-          id={article1.id}
-          img={article1.miniature}
-          description={article1.description}
-          date={date2}
-        />
+        {typeof firstMoreArticle.id === 'number' && (
+          <CardArticle
+            id={firstMoreArticle.id}
+            img={firstMoreArticle.miniature}
+            description={firstMoreArticle.description}
+            date={date2}
+          />
+        )}
 
-        <CardArticle
-          id={article2.id}
-          img={article2.miniature}
-          description={article2.description}
-          date={date3}
-        />
+        {typeof secondMoreArticle.id === 'number' && (
+          <CardArticle
+            id={secondMoreArticle.id}
+            img={secondMoreArticle.miniature}
+            description={secondMoreArticle.description}
+            date={date3}
+          />
+        )}
       </section>
     </>
   );
@@ -86,31 +102,42 @@ export async function getServerSideProps(context) {
     },
   });
 
-  let article1 = await prisma.articles.findUnique({
-    where: {
-      id: id + 1,
-    },
-  });
-
-  let article2 = await prisma.articles.findUnique({
-    where: {
-      id: id + 2,
-    },
-  });
-
   const serializeArticle = new ObjectSerializer();
 
+  let moreArticle = await prisma.articles.findMany({
+    take: 2,
+    where: {
+      id: {
+        gte: id,
+      },
+    },
+    orderBy: {
+      title: 'desc',
+    },
+  });
+
   article = serializeArticle.serialize(article);
-  article1 = serializeArticle.serialize(article1);
-  article2 = serializeArticle.serialize(article2);
 
   prisma.$disconnect();
 
-  return {
-    props: {
-      article,
-      article1,
-      article2,
-    },
-  };
+  if (moreArticle) {
+    let firstMoreArticle = moreArticle[0];
+    let secondMoreArticle = moreArticle[1];
+    firstMoreArticle = serializeArticle.serialize(firstMoreArticle);
+    secondMoreArticle = serializeArticle.serialize(secondMoreArticle);
+
+    return {
+      props: {
+        article,
+        firstMoreArticle,
+        secondMoreArticle,
+      },
+    };
+  } else {
+    return {
+      props: {
+        article,
+      },
+    };
+  }
 }
