4f35c3d2a45dc8c5841e064937dbd15d266f04aa
Fucao77 <axel.zidane@edu.devinci.fr>
2021-04-30 16:12:54 +0200

    add_apropos_responsive

--- components/qui-sommes-nous/ambition/index.jsx
+++ components/qui-sommes-nous/ambition/index.jsx
@@ -5,13 +5,14 @@ import {
   header_contenu,
   blockimage,
   image,
+  texte,
 } from './ambition.module.scss';
 
 export default function Ambition() {
   return (
     <div className={contenu}>
       <div className={header_contenu}>
-        <h2>Notre Histoire</h2>
+        <h2>Notre Amibition</h2>
         <div className={liner}></div>
       </div>
 
@@ -26,6 +27,46 @@ export default function Ambition() {
           alt=""
           className={image}
         />
+      </div>
+
+      <div className={texte}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci a
+          vitae asperiores aliquid. Corrupti exercitationem unde alias
+          voluptatem voluptates dicta accusantium voluptatum ullam architecto,
+          laborum non temporibus cum dignissimos debitis! Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Adipisci a vitae asperiores
+          aliquid. Corrupti exercitationem unde alias voluptatem voluptates
+          dicta accusantium voluptatum ullam architecto, laborum non temporibus
+          cum dignissimos debitis! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Adipisci a vitae asperiores aliquid. Corrupti
+          exercitationem unde alias voluptatem voluptates dicta accusantium
+          voluptatum ullam architecto, laborum non temporibus cum dignissimos
+          debitis!Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Adipisci a vitae asperiores aliquid. Corrupti exercitationem unde
+          alias voluptatem voluptates dicta accusantium voluptatum ullam
+          architecto, laborum non temporibus cum dignissimos debitis!Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Adipisci a vitae
+          asperiores aliquid. Corrupti exercitationem unde alias voluptatem
+          voluptates dicta accusantium voluptatum ullam architecto, laborum non
+          temporibus cum dignissimos debitis!Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Adipisci a vitae asperiores aliquid.
+          Corrupti exercitationem unde alias voluptatem voluptates dicta
+          accusantium voluptatum ullam architecto, laborum non temporibus cum
+          dignissimos debitis!{' '}
+        </p>
+
+        <p>
+          {' '}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci a
+          vitae asperiores aliquid. Corrupti exercitationem unde alias
+          voluptatem voluptates dicta accusantium voluptatum ullam architecto,
+          laborum non temporibus cum dignissimos debitis!Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Adipisci a vitae asperiores
+          aliquid. Corrupti exercitationem unde alias voluptatem voluptates
+          dicta accusantium voluptatum ullam architecto, laborum non temporibus
+          cum dignissimos debitis!
+        </p>
       </div>
     </div>
   );
